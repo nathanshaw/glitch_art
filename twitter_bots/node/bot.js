de0d7c8d91615c4445f523949e51957f8e980a2b
nathanshaw <nathanshawsemail@gmail.com>
2017-03-28 15:14:27 -0700

    got node image bot uploader working

--- twitter_bots/node/bot.js
+++ twitter_bots/node/bot.js
@@ -3,10 +3,40 @@ console.log('Image Bot Starting')
 var Twit = require('twit');
 var config = require('./config.js');
 var T = new Twit(config)
+var exec = require('child_process').exec;
+var fs = require('fs')
 
+function tweetIt() {
+    console.log('Tweeting it')
+    var command = 'processing-java --sketch=`pwd`/image_host --run'
+    exec(command, processing);
+    function processing() {
+        var filename = 'image_host/output.png'
+        var params = {
+            encoding: 'base64'
+        }
+        var content = fs.readFileSync(filename, params)
+        // just uploads does not tweet
+        T.post('media/upload',{media_data:content}, uploaded)
+        function uploaded(err, data, response) {
+            // where the tweet actually happens
+            var id = data.media_id_string;
+            var tweet = {
+                status: '#CTSB',
+                media_ids: [id]
+            }
+            T.post('statuses/update', tweet, tweeted);
+        }
+        function tweeted(err, data, response) {
+            if (err) {
+                console.log("Something went wrong");
+            }else{
+                console.log("It worked!");
+            }
+        }
+        console.log("finished")
+    }
+    console.log('finished tweeting it')
+}
 
-
-
-
-// program is done!!!
-console.log('Image Bot Ended')
+tweetIt();
