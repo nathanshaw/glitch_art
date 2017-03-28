992047899e03bd50eb0fdbf60ac8aa8bf0dbfe74
nathanshaw <nathanshawsemail@gmail.com>
2017-03-28 13:15:01 -0700

    started rewrite of twitter bot in node

---
+++ twitter_bots/node/bot.js
@@ -0,0 +1,12 @@
+console.log('Image Bot Starting')
+// an import statement in node
+var Twit = require('twit');
+var config = require('./config.js');
+var T = new Twit(config)
+
+
+
+
+
+// program is done!!!
+console.log('Image Bot Ended')
