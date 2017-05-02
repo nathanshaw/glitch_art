3b42fe1e86ce8f08b9123d5e05ddd2354a67cbf7
nathanshaw <nathanshawsemail@gmail.com>
2017-05-02 00:27:30 -0700

    made tcsb into a submodule

--- slit_scan_project/blend.glsl
+++
@@ -1,7 +0,0 @@
-varying vec4 vertTexCoord;
-
-void main() {
-    vec2 = vecTexCoord.xy;
-
-    gl_
-}
--- slit_scan_project/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    consumer_key: "YftisSMGITwVf9zyLnfz0HRVz"
-  , consumer_secret: "OCw264vOXl4yCVFmoisZhyxqn3ujHOUM5GapTjL22gGy4IOrcs"
-  , access_token: "846790274500067329-l4RiS4s4JBP0xba355sXQYZj4kAPnA0"
-  , access_token_secret: "07OuQYM5pIMAuQ5o3axQ2HhixbHBPfIRsCLyEGPEs3K3u"
-}
